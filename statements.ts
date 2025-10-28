bdb53d1b171f935a2d5350f7ceb0ed8f2c1c23b6
Praveen Roy <pkroy0505@gmail.com>
2025-10-28 20:16:36 +0530

    added statement

--- statements.ts
+++ statements.ts
@@ -26,3 +26,114 @@ let isDropDownAvl = "notdisplayed"
 if(isDropDownAvl == "displayed"){
     console.log("Click on the drop down")
 }
+
+// 2. if else statement
+
+// Syntax:
+
+/*
+if(condition){
+   // code
+}
+else{
+    // Code
+}
+
+*/
+
+let loginText = "Logged In Failed"
+
+if(loginText === "Logged In Successfully")
+{
+    console.log("Login test passed")
+}
+else{
+    console.log("Login test failed")
+}
+
+
+// 3. if else if statement
+
+// Syntax:
+
+/*
+
+if(condition){
+   // If the condition is true - if block will be executed
+}
+
+else if(condition1)
+{
+   // If the condition1 is true - this if block will be executed
+}
+else if(condition2){
+  // If the condition2 is true - this if block will be executed
+}
+else{
+ //Execute this block of code
+}
+
+
+*/
+
+// Trying to solve a problem of cross browser testing
+
+let browser = "edge"
+
+if(browser == "chrome"){ // "edge" == "chrome"
+    console.log("Launch the chrome browser")
+}
+else if (browser == "firefox"){ // "edge" == "firefox"
+   console.log("Launch the firefox browser");  
+}
+else if (browser == "safari"){ // "edge" == "safari"
+    console.log("Launch the safari browser");
+}
+else {
+    console.log("Invalid browser");
+}
+
+// 4. switch statement
+
+// Syntax:
+
+/*
+
+switch(expression){
+  case value1:
+    // code to be executed
+    break;
+  case value2:
+    // code to be executed
+    break
+   default:
+    //code to be executed
+}
+
+
+*/
+
+let browserName = "safari"
+
+switch(browserName){ // "chrome"
+    case "chrome": // if(browerName == "chrome")
+       console.log("launch the chrome browser")
+       break
+    case "firefox":
+        console.log("launch the firefox browser")
+        break
+    case "safari":
+        console.log("Launch the safari browser");
+        break
+    default:
+        console.log("Invalid browser");
+}
+
+
+// When to use if else if statement
+// 1. Ranges (>90)
+// 2. Logical Operators (&& ||)
+// 3. Mutiple datatype for comparison - name = "rahul", salary > 100000
+
+// When to use switch statement
+// 1. Simple comparision - 1 datatype
