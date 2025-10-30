3a25e0958bdb2d34e416d4bd6d5552410759be3c
Praveen Roy <pkroy0505@gmail.com>
2025-10-30 21:04:56 +0530

    added loops

--- loops.ts
+++ loops.ts
@@ -18,7 +18,7 @@ console.log("**********************");
      // b. for ... of loop - Loop over the iterable objects - Array - [1,2,3,4]
      // c. for ...in loop - Looping over the properties of an Object. {key: value}
 // 2. while loop - If we do not know how many times we have to execute the ieration
-// 3. do while loop - If we want to execute a loop atleast one time bedfor checking the condition
+// 3. do while loop - If we want to execute a loop atleast one time and then checking the condition
 
 
 // 1. for loop
@@ -40,6 +40,78 @@ for(let i=1; i<=5; i++){ // 6<=5 // 4++ = i=5+1 // 6
     console.log(i) // 1 2 3 4 5
 }
 
-// Print all the even number between 1 to 100
+// Print all the even number between 1 to 100 // i+2
 
+console.log("***************************");
+
+// 2. while loop 
+// Syntax - 
+
+
+/*
+
+initialisation
+
+while(condition){
+  // block of code
+  increment/decrement
+}
+
+*/
+
+// 1 to 5
+
+let i =1 
+
+while(i<=5){
+  console.log(i);
+  i++
+}
+console.log("***********************");
+// 10 to 1 using while loop
+
+let j = 10
+
+while(j<1){ // 0>=1
+  console.log(j); // 10 9 8 7 6 5 4 3 2 1
+  j--
+}
+
+// Calendar selection
+// Selecting a product from page using pagination
+// Custom logic to wait for an element to be displayed and then perform the action
+
+console.log("**********************");
+// 3. do while loop
+// Syntax:
+
+/*
+
+// initialisation
+
+do
+{
+ // block of code
+ increment/decrement
+}while(condition);
+
+*/
+
+let k = 10
+
+do
+{
+  console.log(k);
+  k--
+}while(k<1)
+
+
+  // develope an program which takes the order from the user for reastaurant and display the price for all the orders
+
+
+// function
+// Array
+// String - methods
+// Objects
+// Class
 
